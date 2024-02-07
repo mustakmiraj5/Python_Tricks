f10abec856517891cb132290355c74cb423a5799
mustakmiraj5 <wildmiraj@gmail.com>
2024-02-07 10:03:09 +0600

    sort dict in reverse

--- sort_python_dict_by_value.py
+++ sort_python_dict_by_value.py
@@ -11,3 +11,21 @@ print(sorted(xs.items(), key=lambda x: x[1]))
 import operator
 print(sorted(xs.items(), key=operator.itemgetter(1)))
 # [('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+
+
+# Reverse sort by keys:
+my_dict = {'a': 3, 'b': 1, 'c': 2}
+
+# Reverse sort by keys
+sorted_dict_by_keys = dict(sorted(my_dict.items(), key=lambda x: x[0], reverse=True))
+
+print(sorted_dict_by_keys)
+
+
+# Reverse sort by values:
+my_dict = {'a': 3, 'b': 1, 'c': 2}
+
+# Reverse sort by values
+sorted_dict_by_values = dict(sorted(my_dict.items(), key=lambda x: x[1], reverse=True))
+
+print(sorted_dict_by_values)
